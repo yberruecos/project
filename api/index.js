7abdc4da48d83604d453f220c954cd4eb1b145d7
Yonatan Berruecos Orozco <yonatan.berruecos@globant.com>
2022-04-02 17:00:24 -0500

    Fixed node server

--- api/index.js
+++
@@ -1,8 +0,0 @@
-const express=require('express')
-const router=require('../router/router')
-const app=express()
-
-router(app)
-
-
-module.exports=app;
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const app=require('./api/index')
+const express=require('express')
+const router=require('./router/router')
+const app=express()
 const PORT=8080
+
+router(app)
 
 app.listen(PORT,()=>{
     console.log(`Listen port ${PORT}`)
