a681ec5246f33e859ba1f1de6a64ec264d26a32d
Yonatan Berruecos Orozco <yonatan.berruecos@globant.com>
2022-06-20 09:10:42 -0500

    Restore files

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
+// require('@babel/register')({
+//     presets: ['@babel/preset-env', '@babel/preset-vue'],
+//   });
+  
+const dotenv=require('dotenv')
 const express=require('express')
+const cors = require('cors');
 const router=require('./router/router')
 const app=express()
+const db=require('./db')
 const PORT=3000
 
+require('asset-require-hook')({
+    extensions: ['jpg','png'],
+    name: '[hash].[ext]'
+})
+
+// dotenv.config()
+// const {PORT,ENV}=process.env
+
+if(ENV==='develoment'){
+    console.log('ENV DEV')
+}
+
 router(app)
+
+app.use(cors())
+
+app.use(express.static("."));
 
 app.listen(PORT,()=>{
     console.log(`Listen port ${PORT}`)
