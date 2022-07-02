c0d7b0c43be0bccce041afb965d20dac6d4943a9
Yonatan Berruecos Orozco <yonatan.berruecos@globant.com>
2022-07-02 13:40:20 -0500

    heroku setup

--- index.js
+++ index.js
@@ -2,20 +2,20 @@
 //     presets: ['@babel/preset-env', '@babel/preset-vue'],
 //   });
   
-// const dotenv=require('dotenv')
+const dotenv=require('dotenv')
 const express=require('express')
 const cors = require('cors');
 const router=require('./router/router')
 const app=express()
 const db=require('./db')
-const PORT=8000
+// const PORT=3000
 
 // require('asset-require-hook')({
 //     extensions: ['jpg','png'],
 //     name: '[hash].[ext]'
 // })
 
-// dotenv.config()
+dotenv.config()
 // const {PORT,ENV}=process.env
 
 // if(ENV==='develoment'){
@@ -26,10 +26,15 @@ router(app)
 
 app.use(cors())
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
 // app.use(express.static("."));
 
-app.listen(PORT,()=>{
-    console.log(`Listen port ${PORT}`)
+app.listen(port,()=>{
+    console.log(`Listen port ${port}`)
 })
 
 
